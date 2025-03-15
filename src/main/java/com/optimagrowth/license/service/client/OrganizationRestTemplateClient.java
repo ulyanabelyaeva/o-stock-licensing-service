d7b9e40340378460b9e3822c8f0b3b8e2aec9c6f
Беляева Ульяна Дмитриевна <Belyaeva@innovasystem.pro>
2025-03-15 20:06:09 +0300

    add logback conf; add sleuth

--- src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
+++ src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
@@ -5,6 +5,8 @@ import com.optimagrowth.license.repository.OrganizationRedisRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.sleuth.ScopedSpan;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -17,17 +19,20 @@ public class OrganizationRestTemplateClient {
 
     private final OrganizationRedisRepository organizationRedisRepository;
     private final RestTemplate restTemplate;
+    private final Tracer tracer;
 
     public OrganizationRestTemplateClient(OrganizationRedisRepository organizationRedisRepository,
-                                          RestTemplate restTemplate) {
+                                          RestTemplate restTemplate,
+                                          Tracer tracer) {
         this.organizationRedisRepository = organizationRedisRepository;
         this.restTemplate = restTemplate;
+        this.tracer = tracer;
     }
 
     @CircuitBreaker(name = "organizationService")
     public Organization getOrganization(String organizationId) {
         Organization organization = this.checkRedisCache(organizationId);
-        if (organization != null){
+        if (organization != null) {
             logger.debug("Successfully retrieved by id {} from the redis cache: {}", organizationId, organization);
             return organization;
         }
@@ -47,18 +52,23 @@ public class OrganizationRestTemplateClient {
     }
 
     private Organization checkRedisCache(String organizationId) {
+        ScopedSpan newSpan = tracer.startScopedSpan("readLicensingDataFromRedis");
         try {
             return organizationRedisRepository.findById(organizationId).orElse(null);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error("Error when trying to retrieve organization {} check Redis Cache. Exception {}", organizationId, ex);
             return null;
+        } finally {
+            newSpan.tag("peer.service", "redis");
+            newSpan.name("Client received");
+            newSpan.end();
         }
     }
 
     private void cacheOrganizationObject(Organization organization) {
         try {
             organizationRedisRepository.save(organization);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error("Unable to cache organization {} in Redis. Exception {}", organization.getId(), ex);
         }
     }
